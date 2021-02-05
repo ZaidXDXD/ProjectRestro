b4cfdab39be43f5472dec77c793ac8713133631a
Zaid <zaidkhanxdxd@gmail.com>
2021-02-06 00:16:39 +0530

    Added Login And Logout Page

--- ProjectRestro/accounts/urls.py
+++ ProjectRestro/accounts/urls.py
@@ -1,9 +1,15 @@
 from django.urls import path
 from accounts.views import (
     signup,
+    loginpage,
+    logoutuser,
 )
 
 
 urlpatterns = [
     path('register/', signup, name="signup"),
+
+    path('login/', loginpage, name="login"),
+
+    path('logout/', logoutuser, name='logout')
 ] 
--- ProjectRestro/accounts/views.py
+++ ProjectRestro/accounts/views.py
@@ -1,4 +1,7 @@
+from django.contrib import messages
 from django.contrib.auth import login as auth_login
+from django.contrib.auth import logout
+from django.contrib.auth.models import User
 from django.shortcuts import redirect, render
 
 from .forms import SignUpForm
@@ -14,3 +17,27 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'accounts/signup.html', {'form' : form})
+
+
+def loginpage(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = User.objects.filter(username=username).first()
+        if user is None:
+            user = User.objects.filter(email=username.lower()).first()
+        if user is not None:
+            if user.check_password(raw_password=password):
+                auth_login(request, user)
+                return redirect('home') 
+            else:
+                messages.info(request, "Please Check Your Usename And Password They Were Incorrect!")
+        else:
+            messages.info(request, "Please Check Your Usename And Password They Were Incorrect!")    
+    return render(request, 'accounts/login.html')
+
+
+
+def logoutuser(request):
+    logout(request)
+    return redirect('login')
