9be4072009e03004cf1000cffb57fbb5dc96a99d
Zaid <zaidkhanxdxd@gmail.com>
2021-02-06 12:04:47 +0530

    tags complete

--- ProjectRestro/home/urls.py
+++ ProjectRestro/home/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from home.views import home
+from home.views import home, tagpage
 
 urlpatterns = [
     path("", home, name='home'),
+
+    path("add_tag/",tagpage, name="addtag"),
 ]
--- ProjectRestro/home/views.py
+++ ProjectRestro/home/views.py
@@ -1,4 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from .forms import TagForm
 
 def home(request):
     return render(request, 'home/home.html')
+
+
+# View For Tags
+
+
+def tagpage(request):
+    if request.method == "POST":
+        form = TagForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = TagForm()
+    return render(request, 'home/tag.html', {'form':form})
