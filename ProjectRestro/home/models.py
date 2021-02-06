cc10e9dc9f982fb3794f72c999fa8df9a7589db2
Zaid <zaidkhanxdxd@gmail.com>
2021-02-06 11:40:16 +0530

    Added Tag Model

--- ProjectRestro/home/models.py
+++ ProjectRestro/home/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Tag(models.Model):
+    name = models.CharField(max_length=255, null=True, unique=True)
+    def __str__(self):
+        return self.name
